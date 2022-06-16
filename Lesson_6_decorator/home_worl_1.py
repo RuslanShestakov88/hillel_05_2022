61b04dcddb47b8c66b96f4c750408b390ad344c1
RuslanShestakov88 <rshestakov88@gmail.com>
2022-06-17 00:22:52 +0300

    Lesson_6_home_work

---
+++ Lesson_6_decorator/home_worl_1.py
@@ -0,0 +1,17 @@
+def reverse(func):
+    def wrapper(x):
+        if isinstance(x, str):
+            result = x[::-1]
+        else:
+            result = None
+        return result
+    return wrapper
+
+
+
+@reverse
+def primer(x):
+    return x
+
+print(primer("3ffff2"))
+print(primer(444))
