5ae77a416688c586c88eb735b9bb53be1eb618e7
RuslanShestakov88 <rshestakov88@gmail.com>
2022-06-17 23:54:56 +0300

    1

--- Lesson_4_iter_gener_functions/home_work_less_4.py
+++ Lesson_4_iter_gener_functions/home_work_less_4.py
@@ -1,8 +1,10 @@
 from tkinter import Spinbox
 from typing import Generator
+from unittest import result
 FILENAME = "rockyou.txt"
 SEARCH_KEYWORD = "user123"
 
+results = []
 def read_lines_find_admin_generator() -> Generator:
     
     with open(FILENAME, encoding="utf-8") as file:
@@ -12,12 +14,11 @@ def read_lines_find_admin_generator() -> Generator:
                 break
             if SEARCH_KEYWORD in line:
                 yield line.replace("\n", "")
+                results.append(line)
                 continue
+        return results
 
-question = input ("dobavlyat?")
-spisok = []
-for line in read_lines_find_admin_generator():
-    print(line)
-    if question == ("y"):
-        spisok.append(line)
-###
+
+
+#for line in read_lines_find_admin_generator():
+#    print(line)
