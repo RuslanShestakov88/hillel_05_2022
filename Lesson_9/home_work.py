cd2f467819d0c50fb808056aa211845bf3f2ead9
RuslanShestakov88 <rshestakov88@gmail.com>
2022-06-24 17:06:32 +0300

    lesson_9_home_work_1/2

---
+++ Lesson_9/home_work.py
@@ -0,0 +1,19 @@
+class Price:
+    def __init__(self, amount: int, currency: str) -> None:
+        self.amount = amount
+        self.currency = currency
+
+    def __str__(self) -> str:
+        return f"price is {self.amount} {self.currency}"
+
+    def __add__(self, other: "Price") -> "Price":
+        results = Price(amount=self.amount + other.amount, currency=self.currency)
+        return results
+
+
+a = Price(13, "usd")
+b = Price(300, "usd")
+c = a + b
+print(a)
+print(b)
+print(c)
